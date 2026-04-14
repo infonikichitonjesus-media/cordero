f4618b1ce70e870d337497a2b4f2ae288cb0bd8d
infonikichitonjesus-media <info.nikichitonjesus@gmail.com>
2026-04-13 19:16:19 -0600

    Create aventura-galactica.js

---
+++ data/libros/aventura-galactica.js
@@ -0,0 +1,13 @@
+export default {
+    id: "aventura-galactica",
+    titulo: "Aventura Galáctica",
+    autor: "S. K. Miller",
+    descripcion: "Un piloto rebelde lucha por la libertad en el borde de la galaxia.",
+    portada: "https://picsum.photos/id/29/200/260",
+    precio: 4.99,
+    genero: "Ciencia Ficción",
+    pageCount: 42,
+    pageBaseUrl: null,
+    pdf_url: "#",
+    audio_url: "#"
+};
