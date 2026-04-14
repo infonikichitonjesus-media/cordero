031674a72f1b14936f864d6babd491e364e40485
infonikichitonjesus-media <info.nikichitonjesus@gmail.com>
2026-04-13 19:50:26 -0600

    Create no-os-dejare.js

---
+++ data/libros/no-os-dejare.js
@@ -0,0 +1,13 @@
+export default {
+    id: "no-os-dejare",
+    titulo: "No os dejaré Huérfanos",
+    autor: "Xhaal Anay",
+    descripcion: "Un piloto rebelde lucha por la libertad en el borde de la galaxia.",
+    portada: "https://archive.org/download/corderono-os-dejare/1.png",
+    precio: 4.99,
+    genero: "Espiritualidad",
+    pageCount: 42,
+    pageBaseUrl: "https://archive.org/download/corderono-os-dejare/",
+    pdf_url: "#",
+    audio_url: "#"
+};
