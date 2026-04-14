c438188b4cae3975f5fb999b2b82dffd3fc2ccc5
infonikichitonjesus-media <info.nikichitonjesus@gmail.com>
2026-04-13 19:15:42 -0600

    Create el-misterio.js

---
+++ data/libros/el-misterio.js
@@ -0,0 +1,13 @@
+export default {
+    id: "el-misterio",
+    titulo: "El Misterio del Templo",
+    autor: "Elena Vance",
+    descripcion: "Una arqueóloga descubre un antiguo secreto que cambiará la historia.",
+    portada: "https://picsum.photos/id/20/200/260",
+    precio: 0, // gratis
+    genero: "Misterio",
+    pageCount: 24,
+    pageBaseUrl: null, // usar generador local
+    pdf_url: null,
+    audio_url: null
+};
