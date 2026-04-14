1c89bc03c8aa3271dc679a4ef595b7ccc2d663ce
infonikichitonjesus-media <info.nikichitonjesus@gmail.com>
2026-04-13 19:16:40 -0600

    Create poesias-del-alma.js

---
+++ data/libros/poesias-del-alma.js
@@ -0,0 +1,12 @@
+export default {
+    id: "poesias-del-alma",
+    titulo: "Poesías del Alma",
+    autor: "Clara Luz",
+    descripcion: "Versos que tocan el corazón y el espíritu.",
+    portada: "https://picsum.photos/id/96/200/260",
+    precio: 0,
+    genero: "Poesía",
+    pageCount: 18,
+    pageBaseUrl: null,
+    pdf_url: null
+};
